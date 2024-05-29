6b8aaf1eae434b3e19ab816d721c95da04bd1dc8
krishn404 <maharshikrishnakant@gmail.com>
2024-05-29 18:25:29 +0530

    "Updated counter values in index.html and added delay to animation in script.js"

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
-  $('.counter-box').each(function() {
-      var $this = $(this),
-          countTo = $this.attr('data-to');
-
-      $({
-          countNum: $this.find('.counter-value').text()
-      }).animate({
-          countNum: countTo
-      }, {
-          duration: 3000,
-          easing: 'linear',
-          step: function() {
-              $this.find('.counter-value').text(Math.floor(this.countNum) + ' +');
-          },
-          complete: function() {
-              $this.find('.counter-value').text(this.countNum + ' +');
-          }
-      });
-  });
-});
+    $('.counter-box').each(function() {
+        var $this = $(this),
+            countTo = $this.attr('data-to');
+  
+        $({
+            countNum: $this.find('.counter-value').text()
+        }).delay(1000).animate({
+            countNum: countTo
+        }, {
+            duration: 3000,
+            easing: 'linear',
+            step: function() {
+                $this.find('.counter-value').text(Math.floor(this.countNum) + ' +');
+            },
+            complete: function() {
+                $this.find('.counter-value').text(this.countNum + ' +');
+            }
+        });
+    });
+  })
